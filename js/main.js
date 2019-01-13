6617eb2a36740ca7a7856b7b1b22fd1fb9eef7d0
amadeousss <amady833@gmail.com>
2019-01-13 20:21:00 +0300

    everything but shaking

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const jedi = document.querySelector("#jedi");
+const sith = document.querySelector("#sith");
+const saber = document.querySelector(".container");
+const GREEN = "#2ecc71";
+const RED = "#d91e18";
+
+jedi.addEventListener("click", () => {
+  saber.style.background = GREEN;
+  TweenMax.to(".overflow h1", 1, {
+    opacity: 0,
+    y: -60,
+  });
+  TweenMax.to(".overflow", 1, {
+    delay: 0.2,
+    top: "-100%",
+    ease: Expo.easeInOut
+  });
+})
+
+sith.addEventListener("click", () => {
+  saber.style.background = RED;
+  TweenMax.to(".overflow h1", 1, {
+    opacity: 0,
+    y: -60,
+  });
+  TweenMax.to(".overflow", 1, {
+    delay: 0.2,
+    top: "-100%",
+    ease: Expo.easeInOut
+  });
+})
