c0182add95ed99adda71a62538fd4b746b4ab0e8
Amadeousss <amady833@gmail.com>
2019-12-17 22:19:41 +0300

    syllables support

--- Rhymes/main.js
+++ Rhymes/main.js
@@ -5,6 +5,9 @@ const find = document.querySelector('#find');
 let words;
 let found = [];
 let j = 0;
+let syllables = ['а', 'и', 'о', 'у', 'я', 'ё', 'ю', 'ы', 'е', 'э'];
+
+
 
 $.get('text.txt', function (data) {
     words = data.split(' ');
@@ -15,7 +18,7 @@ $.get('text.txt', function (data) {
 find.addEventListener('click', () => {
     if (textInput.value && numberInput.value > 0){
         for (let i = Math.abs(Math.floor(Math.random() * (words.length) - 500)), k = i; (i < words.length - 1 && j < numberInput.value); i++) {
-            if (textInput.value.split('').reverse().join('') == words[i].substr(0, textInput.value.length)){
+            if (textInput.value.split('').reverse().join('').substr(0, getSyllable(textInput.value)) == words[i].substr(0, getSyllable(textInput.value))){
                 j++;
                 found.push(words[i]);
                 console.log(found);
@@ -27,4 +30,13 @@ find.addEventListener('click', () => {
     j = 0;
     found = [];
 })
+
+let getSyllable = function(str){
+    str = str.split('').reverse();
+    for(let i = 1; i < str.length; i ++){
+        if (syllables.includes(str[i])){
+            return i + 1
+        };
+    }
+}
 
