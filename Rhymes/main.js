70b8ada9a92f6367929e3ebc862b652fcccfe4b9
Amadeousss <amady833@gmail.com>
2019-12-19 21:40:45 +0300

    fixed some bugs

--- Rhymes/main.js
+++ Rhymes/main.js
@@ -6,8 +6,7 @@ let words;
 let found = [];
 let j = 0;
 let syllables = ['а', 'и', 'о', 'у', 'я', 'ё', 'ю', 'ы', 'е', 'э'];
-
-
+let attempts = 0;
 
 $.get('text.txt', function (data) {
     words = data.split(' ');
@@ -16,14 +15,23 @@ $.get('text.txt', function (data) {
 }, "text");
 
 find.addEventListener('click', () => {
-    if (textInput.value && numberInput.value > 0){
-        for (let i = Math.abs(Math.floor(Math.random() * (words.length) - 500)), k = i; (i < words.length - 1 && j < numberInput.value); i++) {
-            if (textInput.value.split('').reverse().join('').substr(0, getSyllable(textInput.value)) == words[i].substr(0, getSyllable(textInput.value))){
-                j++;
-                found.push(words[i]);
-                console.log(found);
+    if (textInput.value && numberInput.value > 0) {
+        textLength = getSyllable(textInput.value);
+        let reversedPartOfInput = textInput.value.split('').reverse().join('').substr(0, textLength);
+        while (attempts < 100) {
+            for (let i = Math.abs(Math.floor(Math.random() * (words.length) - 500));
+                (i < words.length - 1 && j < numberInput.value); i++) {
+                let reversedPartOfWord = words[i].substr(0, textLength); //Последний слог слова из массива
+                if (reversedPartOfInput == reversedPartOfWord && !found.includes(words[i])) {
+                    j++;
+                    found.push(words[i]);
+                    console.log(found + " " + j);
+                }
             }
+            attempts++;
+            console.log(attempts);
         }
+        attempts = 0;
     };
     result.innerHTML = '';
     result.innerHTML = found.map(a => a.split('').reverse().join('')).join(', ');
@@ -31,12 +39,14 @@ find.addEventListener('click', () => {
     found = [];
 })
 
-let getSyllable = function(str){
+let getSyllable = function (str) {
     str = str.split('').reverse();
-    for(let i = 1; i < str.length; i ++){
-        if (syllables.includes(str[i])){
-            return i + 1
+    for (let i = 1; i < str.length; i++) {
+        if (syllables.includes(str[i])) {
+            if (i == 1){
+                return i+1;
+            }
+            return i - 1;
         };
     }
 }
-
