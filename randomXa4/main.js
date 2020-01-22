e542344ea41c7fbdcb2ffd1a64f523c155fa8e15
Amadeousss <amady833@gmail.com>
2020-01-22 23:29:11 +0300

    name Generator added 'Хан'

--- randomXa4/main.js
+++ randomXa4/main.js
@@ -8,17 +8,21 @@ let generate = function () {
     let result = '';
     let n = Math.floor(Math.random() * 2 + 4);
     let isVowels = Math.round(Math.random());
-    while (n > 0) {
-        n--;
-        if (isVowels){
-            result += vowels[Math.floor(Math.random() * vowels.length)];
-        } else {
-            result += consonants[Math.floor(Math.random() * consonants.length)]
+        while (n > 0) {
+            n--;
+            if (isVowels) {
+                result += vowels[Math.floor(Math.random() * vowels.length)];
+            } else {
+                result += consonants[Math.floor(Math.random() * consonants.length)]
+            }
+            isVowels = !isVowels
         }
-        isVowels = !isVowels
+    let ending = Math.round(Math.random()) == 1 ? 'бек' : 'хан';
+    console.log(ending);
+    result = result[0].toUpperCase() + result.slice(1) + ending;
+    if (result[0] == 'Ы' || result[0] == 'Ё'){
+        result = result[1].toUpperCase() + result.slice(2);
     }
-    
-    result = result[0].toUpperCase() + result.slice(1) + 'бек';
     let newRes = document.createElement('p')
     newRes.innerHTML = result
     console.log(newRes.innerHTML)
