411663e452f6a5ca8d1b449d772618ccc52c048f
amadeousss <amady833@gmail.com>
2020-06-11 15:18:43 +0300

    image generator

---
+++ demotivator/index.js
@@ -0,0 +1,26 @@
+let upperInput = document.querySelector('#upperText')
+let lowerInput = document.querySelector('#lowerText')
+let upperText = document.querySelector('#upper_meme_text')
+let lowerText = document.querySelector('#lower_meme_text')
+let fileInput = document.querySelector('input[type=file]')
+let img = document.querySelector('img')
+let button = document.querySelector('button')
+
+upperInput.addEventListener('input', () => {
+  upperText.innerText = upperInput.value
+})
+
+lowerInput.addEventListener('input', () => {
+  lowerText.innerText = lowerInput.value
+})
+
+fileInput.addEventListener('change', (event) => {
+  img.src = window.URL.createObjectURL(event.target.files[0]);
+})
+
+button.addEventListener('click', () => {
+    html2canvas(document.querySelector("#meme_picture")).then(canvas => {
+    document.body.innerHTML = '';
+    document.body.appendChild(canvas)
+  })
+})
